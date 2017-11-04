3e1fe6923e02297411338174114ded9c3524ace3
Girts <gist@ntnu.no>
2017-11-04 18:45:06 +0100

    Adding tests to Gemini (currently empty)

---
+++ sandbox/gemini-crawler/src/test/java/org/progfun/GeminiTest.java
@@ -0,0 +1,13 @@
+package org.progfun;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GeminiTest {
+
+    // Test a realistic scenario with predefined messages stored in a file
+    @Test
+    public void testScenario() {
+        
+    }
+}
