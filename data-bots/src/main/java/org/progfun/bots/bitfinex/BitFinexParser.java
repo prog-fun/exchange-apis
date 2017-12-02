247b9fdce4ada37aa6d2460c202664495cf05af8
girts <girts.strazdins@gmail.com>
2017-12-02 02:40:53 +0100

    BitFinex parser error fixed: it was crashing on heartbeat message

--- data-bots/src/main/java/org/progfun/bots/bitfinex/BitFinexParser.java
+++ data-bots/src/main/java/org/progfun/bots/bitfinex/BitFinexParser.java
@@ -11,14 +11,14 @@ import org.progfun.connector.AbstractParser;
  * @author olavt
  */
 public class BitFinexParser extends AbstractParser {
-    
+
     private final int GET_VERSION = 0;
     private final int SUBSCRIBE = 1;
     private final int SNAPSHOT = 2;
     private final int UPDATING = 3;
 
     private int state = 0;
-    
+
     @Override
     public void onMessage(String message) {
 //        System.out.println("Received: " + message);
@@ -29,7 +29,7 @@ public class BitFinexParser extends AbstractParser {
     public void onError(Exception excptn) {
         System.out.println("Error: " + excptn.getMessage());
     }
-    
+
     private void bitFinexWSClientStateMachine(String message) {
 
         switch (state) {
@@ -56,37 +56,48 @@ public class BitFinexParser extends AbstractParser {
 
         }
     }
-    
+
     private void parseAndUpdate(String message) {
         try {
             JSONArray data = new JSONArray(message);
-            JSONArray values = data.getJSONArray(1);
-            Decimal price = new Decimal(values.getDouble(0));
-            int count = values.getInt(1);
-            Decimal amount = new Decimal(values.getDouble(2));
-//            System.out.println("Price: " + price + ", Count: " + count + ", Amount: " + amount);
-            if (count > 0) {
-                // BitFinex always reports the total updated amount, 
-                // not the difference. Therefore we must first remove the
-                // old order and then add it
-                if (amount.isPositive()) {
-                    market.removeBid(price);
-                    market.addBid(price, amount, count);
-                } else if (amount.isNegative()) {
-                    market.removeAsk(price);
-                    market.addAsk(price, amount.negate(), count);
+            Object val = data.get(1);
+            if (val instanceof String) {
+                // This may be a heartbeat message
+                String sv = (String) val;
+                if ("hb".equals(sv)) {
+                    heartbeatReceived();
+                    return;
                 }
-            } else if (count == 0) {
-                if (amount.equals(Decimal.ONE)) {
-                    market.removeBid(price);
-                } else if (amount.negate().equals(Decimal.ONE)) {
-                    market.removeAsk(price);
+            } else if (val instanceof JSONArray) {
+                JSONArray values = (JSONArray) val;
+                Decimal price = new Decimal(values.getDouble(0));
+                int count = values.getInt(1);
+                Decimal amount = new Decimal(values.getDouble(2));
+//            System.out.println("Price: " + price + ", Count: " + count + ", Amount: " + amount);
+                if (count > 0) {
+                    // BitFinex always reports the total updated amount, 
+                    // not the difference. Therefore we must first remove the
+                    // old order and then add it
+                    if (amount.isPositive()) {
+                        market.removeBid(price);
+                        market.addBid(price, amount, count);
+                    } else if (amount.isNegative()) {
+                        market.removeAsk(price);
+                        market.addAsk(price, amount.negate(), count);
+                    }
+                } else if (count == 0) {
+                    if (amount.equals(Decimal.ONE)) {
+                        market.removeBid(price);
+                    } else if (amount.negate().equals(Decimal.ONE)) {
+                        market.removeAsk(price);
+                    }
                 }
             }
             // TODO - test if parsing works correctly
         } catch (JSONException e) {
-            System.out.println("Error in BitFinex update parsing:" 
+            System.out.println("Error in BitFinex update parsing:"
                     + e.getMessage());
+            System.out.println("Received msg: " + message);
         }
 
 //        System.out.println("Ask: " + market.getAsks().size());
@@ -112,5 +123,13 @@ public class BitFinexParser extends AbstractParser {
 //        System.out.println(market.getAsks().size());
 //        System.out.println(market.getBids().size());
     }
-    
+
+    /**
+     * Heart-beat message received from the server
+     */
+    private void heartbeatReceived() {
+        // TODO - Reset alarm timer
+        System.out.println("Heartbeat received");
+    }
+
 }
