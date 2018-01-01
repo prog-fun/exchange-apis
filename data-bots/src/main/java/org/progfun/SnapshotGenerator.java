d83fdc22fd9bcae6cd8c71ae0ae0f4f557b3744a
girts <girts.strazdins@gmail.com>
2018-01-02 00:56:30 +0100

    Bugfix: SnapshotGenerator was crashing on restart

--- data-bots/src/main/java/org/progfun/SnapshotGenerator.java
+++ data-bots/src/main/java/org/progfun/SnapshotGenerator.java
@@ -12,21 +12,17 @@ public class SnapshotGenerator {
     private SnapshotListener listener;
     private Timer timer;
     private final boolean deleteTrades;
-    
+
     /**
      * Create a new snapshot generator
+     *
      * @param deleteTrades when true, trades will be cleared after each snapshot
      */
     public SnapshotGenerator(boolean deleteTrades) {
         this.deleteTrades = deleteTrades;
     }
-    
-    private final TimerTask task = new TimerTask() {
-        @Override
-        public void run() {
-            notifyListener();
-        }
-    };
+
+    private TimerTask task;
 
     public void setExchange(Exchange exchange) {
         this.exchange = exchange;
@@ -44,6 +40,14 @@ public class SnapshotGenerator {
     public void schedule(long interval) {
         stop(); // Stop previous timer if one running
         timer = new Timer(true);
+        // We must create new task every time. Otherwise we get 
+        // "IllegalStateException" the 2nd time we schedule the same task
+        task = new TimerTask() {
+            @Override
+            public void run() {
+                notifyListener();
+            }
+        };
         timer.scheduleAtFixedRate(task, interval, interval);
         Logger.log("Snapshot generator started");
     }
