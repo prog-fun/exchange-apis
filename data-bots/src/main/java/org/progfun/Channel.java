7ef69880a9af5c123b51edef130427ebe16914bd
girts <girts.strazdins@gmail.com>
2018-07-30 23:00:36 +0200

    Create Channel from String

--- data-bots/src/main/java/org/progfun/Channel.java
+++ data-bots/src/main/java/org/progfun/Channel.java
@@ -4,7 +4,7 @@ package org.progfun;
  * Subscription Channels
  */
 public enum Channel {
-    NONE, 
+    NONE,
     ORDERBOOK, // Orderbook updates (bids/asks)
     TRADES, // Last trade updates
     PRICES_1MIN, // Price (candle) updates: 1min resolution
@@ -19,5 +19,45 @@ public enum Channel {
     PRICES_12H, // Price (candle) updates: 12h resolution
     PRICES_1D, // Price (candle) updates: 1 day resolution
     PRICES_1W, // Price (candle) updates: 1 week resolution
-    TICKER // Ticker updates
+    TICKER; // Ticker updates
+
+    /**
+     * Take price resolution string ("5m" etc), convert it to Channel object
+     *
+     * @param res
+     * @return Channel object or null if the resolution string is not supported
+     */
+    public static Channel priceStringToChannel(String res) {
+        if (res == null) {
+            return null;
+        }
+        switch (res.toLowerCase()) {
+            case "1m":
+                return PRICES_1MIN;
+            case "5m":
+                return PRICES_5MIN;
+            case "15m":
+                return PRICES_15MIN;
+            case "30m":
+                return PRICES_30MIN;
+            case "1h":
+                return PRICES_1H;
+            case "2h":
+                return PRICES_2H;
+            case "3h":
+                return PRICES_3H;
+            case "4h":
+                return PRICES_4H;
+            case "6h":
+                return PRICES_6H;
+            case "12h":
+                return PRICES_12H;
+            case "1d":
+                return PRICES_1D;
+            case "1w":
+                return PRICES_1W;
+            default:
+                return null;
+        }
+    }
 }
